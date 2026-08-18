d94e548a5fba060a8a137d2aa0ef1978a79ea139
isha341 <isha@theleadershipgazette.com>
2026-08-18 11:02:05 +0530

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import Navbar from '../components/global/navbar/Navbar';
 import Footer from '../components/global/footer/Footer';
 import AboutContent from '../components/about/AboutContent';
+dconst About = () => {
 
-
+  useEffect(() => {
+    // SEO Title
+    document.title = 'About The Leadership Gazette | Global Leadership & Business';
+metaDescription.setAttribute(
+  'content',
+  'Discover The Leadership Gazette, a global leadership platform highlighting MENA leaders, business excellence, HR innovation, and inspiring leadership stories.'
+);
 
 const About = () => {
   return (
